f2fb739fc155f34ed76af7ed58d7eb4dd922897e
AndresMHG <andr3sm1guel@gmail.com>
2026-04-16 22:31:06 -0300

    fux

--- src/components/dashboard/DashboardLayout.vue
+++ src/components/dashboard/DashboardLayout.vue
@@ -87,6 +87,12 @@
           <svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" v-html="item.icon" />
           <span>{{ item.label }}</span>
         </a>
+        <button class="db-bottom-nav-item db-bottom-nav-logout" @click="handleLogout">
+          <svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+          </svg>
+          <span>Sair</span>
+        </button>
       </div>
     </nav>
   </div>
